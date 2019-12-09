87f5a828509046a86997ba332e7bee69939b94e5
supriya <supriya.adhatarao@inria.fr>
2019-12-09 14:07:34 +0000

    removed unwanted functions

--- src/main.js
+++ src/main.js
@@ -40,16 +40,16 @@ app.get('/ui/saveConfiguration', (req, res) => {
       const { pii } = settings;
 
       // read data based on the client_id and save as json
-      const redditData = fs.readFileSync('src/pii/' + pii, 'utf8');
-      const redditArray = redditData.split('\n');
-      console.log('Reddit data loaded with length: ' + redditArray.length);
-      save('redditSimulatorData', JSON.stringify(redditArray));
+      const piiData = fs.readFileSync('src/pii/' + pii, 'utf8');
+      const piiArray = piiData.split('\n');
+      console.log('Data loaded with length: ' + piiArray.length);
+      save('PIIData', JSON.stringify(piiArray));
 
       setSettings(settings)
         .then(() => {
           res.type('html');
           res.send(`
-            <h1>Reddit Simulator Driver Configuration</h1>
+            <h1>**********</h1>
             <p>Data have been successfully loaded into the datastore.</p>
           `);
           res.end();
@@ -61,82 +61,6 @@ app.get('/status', function (req, res) {
   res.send('active');
 });
 
-const redditData = databox.NewDataSourceMetadata();
-redditData.Description = 'Reddit Simulator data';
-redditData.ContentType = 'application/json';
-redditData.Vendor = 'Databox Inc.';
-redditData.DataSourceType = 'redditSimulatorData';
-redditData.DataSourceID = 'redditSimulatorData';
-redditData.StoreType = 'ts/blob';
-
-const driverSettings = databox.NewDataSourceMetadata();
-driverSettings.Description = 'Reddit Simulator driver settings';
-driverSettings.ContentType = 'application/json';
-driverSettings.Vendor = 'Databox Inc.';
-driverSettings.DataSourceType = 'redditSimulatorSettings';
-driverSettings.DataSourceID = 'redditSimulatorSettings';
-driverSettings.StoreType = 'kv';
-
-store.RegisterDatasource(redditData)
-  .then(() => {
-    return store.RegisterDatasource(driverSettings);
-  })
-  .catch((err) => {
-    console.log('Error registering data source:' + err);
-  });
-
-function getSettings() {
-  const datasourceid = 'redditSimulatorSettings';
-  return new Promise((resolve, reject) => {
-    store.KV.Read(datasourceid, 'settings')
-      .then((settings) => {
-        console.log('[getSettings] read response = ', settings);
-        if (Object.keys(settings).length === 0) {
-          //return defaults
-          const settings = RedditSimulatorDefaultSettings;
-          //console.log('[getSettings] using defaults Using ----> ', settings);
-          resolve(settings);
-          return;
-        }
-
-        //console.log('[getSettings]', settings);
-        resolve(settings);
-      })
-      .catch((err) => {
-        const settings = RedditSimulatorDefaultSettings;
-        console.log('Error getting settings', err);
-        console.log('[getSettings] using defaults Using ----> ', settings);
-        resolve(settings);
-      });
-  });
-}
-
-function setSettings(settings) {
-  const datasourceid = 'redditSimulatorSettings';
-  return new Promise((resolve, reject) => {
-    store.KV.Write(datasourceid, 'settings', settings)
-      .then(() => {
-        //console.log('[setSettings] settings saved', settings);
-        resolve(settings);
-      })
-      .catch((err) => {
-        console.log('Error setting settings', err);
-        reject(err);
-      });
-  });
-}
-
-async function save(datasourceid, data) {
-  console.log('Saving pii event::', data);
-  const json = { data };
-  store.TSBlob.Write(datasourceid, json)
-    .then((resp) => {
-      console.log('Save got response ', resp);
-    })
-    .catch((error) => {
-      console.log('Error writing to store:', error);
-    });
-}
 
 //when testing, we run as http, (to prevent the need for self-signed certs etc);
 if (DATABOX_TESTING) {
